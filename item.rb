a489bbb99b185d42cacee83a8aee418e6b6e71af
Abdullah <abdullahkhrief@gmail.com>
2023-07-11 18:06:56 +0300

    have fixed linters

--- item.rb
+++ item.rb
@@ -1,27 +1,20 @@
 class Item
-    attr_accessor :published_date, :genre, :label, :author
-    
-    def initialize(genre, author, label, publish_date, archived: true)
-        @id = Random.rand(1..1000)
-        @genre = genre
-        @author = author
-        @label = label
-        @publish_date = publish_date
-        @archived = archived
-    end
+  attr_accessor :published_date, :genre, :label, :author
 
-    def can_be_archived?()
-        if @publish_date < 10.year.ago
-        true
-        else
-        false
-        end
-    end
+  def initialize(genre, author, label, publish_date, archived: true)
+    @id = Random.rand(1..1000)
+    @genre = genre
+    @author = author
+    @label = label
+    @publish_date = publish_date
+    @archived = archived
+  end
 
-    def move_to_archive()
-        if can_be_archived == true
-            @archived = true
-        end
-    end
+  def can_be_archived?
+    @publish_date < 10.year.ago
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived == true
+  end
 end
-        
