e0440763446812b7788cc4689b810de44571c276
Abdullah <abdullahkhrief@gmail.com>
2023-07-11 23:47:53 +0300

    have added the author class and edited the item file

---
+++ author.rb
@@ -0,0 +1,16 @@
+require_relative "item"
+
+class author
+    attr_accessor :first_name, :last_name, :items,
+    def initialize(first_name, last_name)
+        @first_name = first_name
+        @last_name = last_name
+        @items = []
+    end
+    
+    def add_item(item)
+        @items.push(item)
+        item.add_author(self)
+        item.author = self
+      end
+end
