753860715fb799d8221e55ac8a14b9626facb00b
Abdullah <abdullahkhrief@gmail.com>
2023-07-11 17:53:08 +0300

    Have added can_be_archived method and move_to_archive method

---
+++ item.rb
@@ -0,0 +1,26 @@
+class item
+
+    def initialize(genre, author, label, publish_date, archived: true)
+        @id = Random.rand(1..1000)
+        @genre = genre
+        @author = author
+        @label = label
+        @publish_date = publish_date
+        @archived = archived
+    end
+
+    def can_be_archived?()
+        if @publish_date < 10.year.ago
+        true
+        else
+        false
+        end
+    end
+
+    def move_to_archive()
+        if can_be_archived == true
+            @archived = true
+        end
+    end
+end
+        
