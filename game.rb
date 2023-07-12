150f0cdb70385458d1214dc34daa598639983c9d
Abdullah <abdullahkhrief@gmail.com>
2023-07-12 13:20:39 +0300

    have added the game class and fixed the linters

---
+++ game.rb
@@ -0,0 +1,14 @@
+require_relative 'item'
+class Game < Item
+  attr_accessor :multiplayer, :last_played_at
+
+  def initialize(player, last_played_at)
+    super(id, publish_date)
+    @multiplayer = player
+    @last_played_at = last_played_at
+  end
+
+  def can_be_archived?
+    @last_played_at < 2.year.ago
+  end
+end
