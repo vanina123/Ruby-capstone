f6153190563c953bc54d60e48a8f18cd23e46b84
Abdullah <abdullahkhrief@gmail.com>
2023-07-12 18:08:58 +0300

    have fixed linters

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -3,10 +3,10 @@ require_relative '../author'
 RSpec.describe Author do
   describe '#author' do
     it 'returns return the data of Author class' do
-      author = Author.new("James","Last")
+      author = Author.new('James', 'Last')
 
       expect(author.first_name).to eq('James')
-      expect(author.last_name).to eq("Last")
+      expect(author.last_name).to eq('Last')
     end
   end
 end
