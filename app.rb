07c979f682e5bf2dbca4210e18d890e71ac2dbb1
vanina123 <nanavanina834@gmail>
2023-07-13 10:40:33 +0100

    save data

--- app.rb
+++ app.rb
@@ -7,7 +7,6 @@ require_relative 'music_album'
 require_relative 'game'
 require_relative 'lib/save_data'
 require_relative 'lib/load_data'
-require_relative 'lib/loads_data'
 
 class App
     def initialize
@@ -30,9 +29,6 @@ class App
             puts "title: #{music_album.title} by: #{music_album.artist} Genre: #{music_album.genre.name} spotify: #{music_album.on_spotify}"
         end
     end
-
-    # def list_all_movies
-    # end
 
     def list_all_games
         @games.each do |game|
@@ -72,7 +68,7 @@ class App
         first_name = gets.chomp
         print 'Last name of the Author:'
         last_name = gets.chomp
-        print 'cover_state'
+        print 'cover_state:'
         cover_state = gets.chomp
         print 'genre:'
         genre = gets.chomp
@@ -96,7 +92,7 @@ class App
         artist = gets.chomp
         print 'The genre of the album:'
         genre = gets.chomp
-        print 'is it on spotify?[Y,N]'
+        print 'is it on spotify?[Y,N]:'
          spotify = gets.chomp
          if spotify == "Y" or 'y'
             on_spotify = true
@@ -107,7 +103,7 @@ class App
             music = MusicAlbum.new(title, artist, newGenre, on_spotify)
 
         @music_albums << music
-        print "the music album has been added"
+        puts "the music album has been added"
     end
 
     def add_game
@@ -115,7 +111,7 @@ class App
         title = gets.chomp
         print 'The published date of the game:'
         publish_date = gets.chomp
-        print "is it a multiplayer game?[Y,N]"
+        print "is it a multiplayer game?[Y,N]:"
         answer = gets.chomp
         if answer == "Y" or 'y'
             multiplayer = true
@@ -135,10 +131,89 @@ class App
     def quit
 
         puts 'existing the app. Goodbye'
-    #    save_books(@books)
-    #    save_people(@people)
-    #    save_rentals(@rentals)
+        save_music_albums(@music_albums)
+        save_genre(@genres)
+        save_author(@authors)
+        save_game(@games)
+        save_book(@books)
+        save_label(@labels)
        exit
     end
 
+    def save_music_albums(arr)
+        new_arr = []
+        music_albums.each do |album|
+          obj = {
+            title: album.title,
+            artist: album.artist,
+            genre: album.genre
+          }
+          new_arr << obj
+        end
+        save(new_arr, 'music_albums')
+    end
+
+    def save_genre(arr)
+        new_arr = []
+        genres.each do |genre|
+          obj = {
+            name: genre.name,
+            item: genre.item
+          }
+          new_arr << obj
+        end
+        save(new_arr, 'genre')
+    end
+
+    def save_author(arr)
+        new_arr = []
+        authors.each do |author|
+          obj = {
+            first_name: author.first_name,
+            last_name: author.last_name
+          }
+          new_arr << obj
+        end
+        save(new_arr, 'author')
+    end
+
+    def save_game(arr)
+        new_arr = []
+        games.each do |game|
+          obj = {
+            multiplayer: game.multiplayer,
+            last_played_at: game.last_played_at
+          }
+          new_arr << obj
+        end
+        save(new_arr, 'game')
+    end
+
+    def save_book(arr)
+        new_arr = []
+        books.each do |book|
+          obj = {
+            publish_date: book.publish_date,
+            title: book.title,
+            author: book.author,
+            cover_state: book.cover_state,
+            genre: book.genre
+          }
+          new_arr << obj
+        end
+        save(new_arr, 'book')
+    end
+
+    def save_label(arr)
+        new_arr = []
+        labels.each do |label|
+          obj = {
+            title: label.title,
+            items: label.items.map { |item| { name: item.name } }
+          }
+          new_arr << obj
+        end
+        save(new_arr, 'label')
+    end
+  
 end
