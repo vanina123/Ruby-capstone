2151698fdc63b7c4cd1fa33a092fab77d3ecac75
kayemba <kayembejonathan5@gmail.com>
2023-07-13 08:58:24 +0200

    Add content on main

---
+++ main.rb
@@ -0,0 +1,12 @@
+require_relative 'genre'
+require_relative 'book'
+require_relative 'author'
+require_relative 'label'
+require_relative 'item'
+require_relative 'music_album'
+require_relative 'game'
+
+class Main 
+    OPTIONS = {
+
+}.freeze
